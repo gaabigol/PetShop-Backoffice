04c01776cc22164d8d2807b2189fff88d1cddf99
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-05-27 18:38:57 +0100

    Created: DataService

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+
+export class DataService {
+  public url = 'http://localhost:7188';
+
+  constructor(private http: HttpClient) {}
+
+  public authenticate(data: any) {
+    return this.http.post(`${this.url}/v1/login`, data);
+  }
+
+  public getMonthlySalesChartData() {
+    return this.http.get(`${this.url}/v1/reports/ms`);
+  }
+  public getOrders() {
+    return this.http.get(`${this.url}/v1/orders`);
+  }
+
+  public getOrder(order: string) {
+    return this.http.get(`${this.url}/v1/orders/#{order}`);
+  }
+}
